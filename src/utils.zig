84436eba963d66cf25b2fa13c9c7970d061a12f3
dk949 <56653556+dk949@users.noreply.github.com>
2022-08-03 22:52:11 +0300

    utils: added some more utils

--- src/utils.zig
+++ src/utils.zig
@@ -1,16 +1,39 @@
-pub fn hexToStr(num: anytype) [@sizeOf(@TypeOf(num)) * 2]u8 {
+pub const mem = @import("std").mem;
+
+// TODO: make generic
+pub fn hexToStr(num: usize) [@sizeOf(usize) * 2]u8 {
     comptime if (!@import("std").meta.trait.isIntegral(@TypeOf(num)))
         @compileError("argument to hexToStr has to be an integer, not " ++ @typeName(@TypeOf(num)));
 
-    var n = num;
     var ret: [@sizeOf(@TypeOf(num)) * 2]u8 = undefined;
     @memset(&ret, 0, ret.len);
+    switch (num) {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9 => |n| {
+            ret[0] = '0' + @intCast(u8, n);
+            return ret;
+        },
+        10, 11, 12, 13, 14, 15 => |n| {
+            ret[0] = 'A' - 10 + @intCast(u8, n);
+            return ret;
+        },
+        else => {},
+    }
+    var n = num;
     var i: usize = ret.len - @clz(@TypeOf(num), num) / 4;
     while (n != 0) : (n >>= 4) {
-        ret[@intCast(usize, i - 1)] = '0' + (@truncate(u8, n) & 0x0f);
+        ret[i - 1] = blk: {
+            const d = (@truncate(u8, n) & 0x0f);
+            break :blk if ((d >= 0) and (d <= 9)) '0' + d else 'A' - 10 + d;
+        };
         i -= 1;
     }
     return ret;
+}
+
+test "hex to string" {
+    const expect = @import("std").testing.expect;
+    const testNum: usize = 0x21;
+    try expect(mem.eql(u8, &hexToStr(testNum), "1"));
 }
 
 pub fn enumToStr(comptime e: anytype) []const u8 {
@@ -20,21 +43,74 @@ pub fn enumToStr(comptime e: anytype) []const u8 {
         .name;
 }
 
-// TODO: when There is some allocator interface
-//fn split(allocator: Allocator, str: []const u8, sep: u8) !ArrayList([]const u8) {
-//var out = ArrayList([]const u8).init(allocator);
+test "enum to string" {
+    const expect = @import("std").testing.expect;
 
-//var start: usize = 0;
-//var end: usize = 0;
+    const E = enum { A, B, C };
+    const e = E.A;
+    try expect(mem.eql(u8, enumToStr(e), "A"));
+}
 
-//for (str) |ch| {
-//end += 1;
-//if (ch == sep) {
-//try out.append(str[start .. end - 1]);
-//start = end;
-//}
-//}
+/// Remove the outer most layer of const from T if T is a pointer type.
+/// Note: this does not make a const value modifiable.
+/// *const T -> *T
+///       *T -> *T
+/// *const *const T -> **const T
+pub fn removeConst(comptime T: type) type {
+    return switch (@typeInfo(T)) {
+        .Pointer => blk: {
+            var cpy = @typeInfo(T);
+            cpy.Pointer.is_const = false;
+            break :blk @Type(cpy);
+        },
+        else => |t| @compileError("Cannot remove const from " ++ @typeName(@Type(t))),
+    };
+}
+test "remove const" {
+    const expect = @import("std").testing.expect;
+    try expect(removeConst(*const i32) == *i32);
+    try expect(removeConst([]const i32) == []i32);
+    try expect(removeConst([*]const i32) == [*]i32);
+    try expect(removeConst([:0]const i32) == [:0]i32);
+    try expect(removeConst([*:0]const i32) == [*:0]i32);
+    try expect(removeConst(*const *const i32) == **const i32);
 
-//return out;
-//}
+    try expect(removeConst(*i32) == *i32);
+    try expect(removeConst([]i32) == []i32);
+    try expect(removeConst([*]i32) == [*]i32);
+    try expect(removeConst([:0]i32) == [:0]i32);
+    try expect(removeConst([*:0]i32) == [*:0]i32);
+    try expect(removeConst(**const i32) == **const i32);
+}
 
+/// Add const to the outer most layer of T if T is a pointer type.
+///       *T -> *const T
+/// *const T -> *const T
+/// **const T -> *const *const T
+pub fn addConst(comptime T: type) type {
+    return switch (@typeInfo(T)) {
+        .Pointer => blk: {
+            var cpy = @typeInfo(T);
+            cpy.Pointer.is_const = true;
+            break :blk @Type(cpy);
+        },
+        else => |t| @compileError("Cannot add const to " ++ @typeName(@Type(t))),
+    };
+}
+
+test "add const" {
+    const expect = @import("std").testing.expect;
+    try expect(addConst(*i32) == *const i32);
+    try expect(addConst([]i32) == []const i32);
+    try expect(addConst([*]i32) == [*]const i32);
+    try expect(addConst([:0]i32) == [:0]const i32);
+    try expect(addConst([*:0]i32) == [*:0]const i32);
+    try expect(addConst(**const i32) == *const *const i32);
+
+    try expect(addConst(*const i32) == *const i32);
+    try expect(addConst([]const i32) == []const i32);
+    try expect(addConst([*]const i32) == [*]const i32);
+    try expect(addConst([:0]const i32) == [:0]const i32);
+    try expect(addConst([*:0]const i32) == [*:0]const i32);
+    try expect(addConst(*const *const i32) == *const *const i32);
+}
