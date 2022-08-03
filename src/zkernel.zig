43b82861aaa1cd4dfef2889becb5a0fef61080b0
dk949 <56653556+dk949@users.noreply.github.com>
2022-08-03 22:53:26 +0300

    kmain: print some useful info on startup

--- src/zkernel.zig
+++ src/zkernel.zig
@@ -1,6 +1,28 @@
 const terminal = @import("terminal.zig");
+const builtin = @import("builtin");
+const cpu = builtin.cpu;
+const utils = @import("utils.zig");
 
 export fn kmain() void {
-    terminal.initialize();
-    terminal.write("Hello, Kernel World from Zig 0.10.0-dev!\n");
+    const ver = builtin.zig_version;
+    terminal.write("Zig ");
+    terminal.writeNum(ver.major);
+    terminal.write(".");
+    terminal.writeNum(ver.minor);
+    terminal.write(".");
+    terminal.writeNum(ver.patch);
+    if (ver.pre) |pre| {
+        terminal.write("-");
+        terminal.write(pre);
+    }
+    if (ver.build) |build| {
+        terminal.write("+");
+        terminal.write(build);
+    }
+    terminal.write(" running on an ");
+    terminal.write(utils.enumToStr(cpu.arch));
+    terminal.write(" ");
+    terminal.write(cpu.model.name);
+    terminal.write(" cpu.\n");
+    terminal.write("\n\n\nyay!!\n");
 }
