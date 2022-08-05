c2d7d2fc9092985be36b39415f14bcc9c2c60c75
dk949 <56653556+dk949@users.noreply.github.com>
2022-08-05 13:44:48 +0300

    build: moved build code out of a struct

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
-const CrossTarget = std.zig.CrossTarget;
+const config = @import("build/config.zig");
 const Mode = std.builtin.Mode;
-const Target = std.Target;
 const Builder = std.build.Builder;
 const FileSource = std.build.FileSource;
 const LibExeObjStep = std.build.LibExeObjStep;
@@ -11,171 +10,139 @@ const PathDependencyStep = @import("build/PathDependencyStep.zig");
 const FsStep = @import("build/FsStep.zig");
 const GenerateFileStep = @import("build/GenerateFileStep.zig");
 
-const OS_NAME = "adelpha";
-
 const ArtifactTag = struct { artifact: *LibExeObjStep };
 
-const Build = struct {
-    const Self = Build;
+var builder: *Builder = undefined;
+var mode: Mode = undefined;
+const arch_str = std.meta.tagName(config.target.cpu_arch.?);
+const qemu = "qemu-system-" ++ arch_str;
+var default_ld: FileSource = undefined;
 
-    builder: *Builder,
-    mode: Mode,
-    target: CrossTarget,
-    arch_str: []const u8 = undefined,
-    qemu: []const u8 = undefined,
-    default_ld: ?FileSource = null,
+fn setDefaultLdOr(leo: *LibExeObjStep, alt: ?FileSource) void {
+    leo.setLinkerScriptPath(alt orelse default_ld);
+}
 
-    fn setDefaultLdOr(self: *Self, leo: *LibExeObjStep, alt: ?FileSource) void {
-        if (self.default_ld orelse alt) |ld|
-            leo.setLinkerScriptPath(ld);
-    }
+fn addObject(name: []const u8, source: []const u8) *LibExeObjStep {
+    var obj = builder.addObject(name, source);
+    obj.setBuildMode(mode);
+    obj.setTarget(config.target);
+    setDefaultLdOr(obj, null);
+    return obj;
+}
 
-    fn addObject(self: *Self, name: []const u8, source: []const u8) *LibExeObjStep {
-        var obj = self.builder.addObject(name, source);
-        obj.setBuildMode(self.mode);
-        obj.setTarget(self.target);
-        self.setDefaultLdOr(obj, null);
-        return obj;
-    }
+fn addExecutable(name: []const u8, source: []const u8) *LibExeObjStep {
+    var exe = builder.addExecutable(name, source);
+    exe.setBuildMode(mode);
+    exe.setTarget(config.target);
+    setDefaultLdOr(exe, null);
+    return exe;
+}
 
-    fn addExecutable(self: *Self, name: []const u8, source: []const u8) *LibExeObjStep {
-        var exe = self.builder.addExecutable(name, source);
-        exe.setBuildMode(self.mode);
-        exe.setTarget(self.target);
-        self.setDefaultLdOr(exe, null);
-        return exe;
-    }
+fn addStep(name: []const u8, description: []const u8) *Step {
+    return builder.step(name, description);
+}
 
-    fn addStep(self: *Self, name: []const u8, description: []const u8) *Step {
-        return self.builder.step(name, description);
-    }
+fn createPathDependencyStep(name: []const u8, deps: ?[]const []const u8) *PathDependencyStep {
+    return PathDependencyStep.create(builder, name, deps);
+}
 
-    fn createPathDependencyStep(self: *Self, name: []const u8, deps: ?[]const []const u8) *PathDependencyStep {
-        return PathDependencyStep.create(self.builder, name, deps);
-    }
+fn createRunStep(name: []const u8) *RunStep {
+    const run_step = RunStep.create(builder, name);
+    return run_step;
+}
 
-    fn createRunStep(self: *Self, name: []const u8) *RunStep {
-        const run_step = RunStep.create(self.builder, name);
-        return run_step;
-    }
+fn createFsStep(name: []const u8, action: FsStep.FsAction) *FsStep {
+    return FsStep.create(builder, name, action);
+}
 
-    fn createFsStep(self: *Self, name: []const u8, action: FsStep.FsAction) *FsStep {
-        return FsStep.create(self.builder, name, action);
-    }
+fn createGenerateFileStep(name: []const u8, file_name: []const u8, file_contents: []const u8) *GenerateFileStep {
+    return GenerateFileStep.create(builder, name, file_name, file_contents);
+}
 
-    fn createGenerateFileStep(self: *Self, name: []const u8, file_name: []const u8, file_contents: []const u8) *GenerateFileStep {
-        return GenerateFileStep.create(self.builder, name, file_name, file_contents);
-    }
-
-    fn joinPath(self: *Self, paths: []const []const u8) []const u8 {
-        return std.fs.path.join(self.builder.allocator, paths) catch unreachable;
-    }
-
-    pub fn create(
-        b: *Builder,
-        mode: Mode,
-        comptime target: CrossTarget,
-        comptime default_ld: ?[]const u8,
-    ) Self {
-        var self = Self{
-            .builder = b,
-            .mode = mode,
-            .target = target,
-            .default_ld = if (default_ld) |ld| FileSource{ .path = ld } else null,
-        };
-        const arch_str = comptime std.meta.tagName(target.cpu_arch.?);
-        self.arch_str = arch_str;
-        self.qemu = "qemu-system-" ++ arch_str;
-        return self;
-    }
-
-    pub fn build(self: *Self) void {
-        const rt = self.addObject("runtime", "src/runtime.zig");
-        const exe = self.addExecutable("kernel", "src/kernel.zig");
-
-        exe.addObject(rt);
-        exe.install();
-
-        const check_qemu = self.createPathDependencyStep("check qemu", &.{self.qemu});
-
-        const run_step = self.createRunStep("run qemu");
-        run_step.addArgs(&.{ self.qemu, "-kernel" });
-        run_step.addArtifactArg(exe);
-        run_step.step.dependOn(&exe.install_step.?.step);
-        run_step.step.dependOn(&check_qemu.step);
-
-        const run = self.addStep("run", "Run the kernel in qemu");
-        run.dependOn(&run_step.step);
-
-        const check_iso_prereqs = self.createPathDependencyStep(
-            "check iso creation prerequisites",
-            &.{ "xorriso", "mformat", "grub-mkrescue" },
-        );
-        _ = check_iso_prereqs;
-
-        const isodir_path = self.joinPath(&.{ self.builder.install_prefix, "isodir" });
-        const isodir_boot_path = self.joinPath(&.{ isodir_path, "boot" });
-        const isodir_grub_path = self.joinPath(&.{ isodir_boot_path, "grub" });
-
-        const mkdir_isodir = self.createFsStep("mkdir isodir", .{
-            .mkdir = .{
-                .path = isodir_grub_path,
-                .parents = true,
-            },
-        });
-
-        const cp_kernel = self.createFsStep("cp kernel", .{
-            .cp = .{
-                .to = self.joinPath(&.{ isodir_boot_path, OS_NAME ++ ".bin" }),
-                .from = self.joinPath(&.{ self.builder.install_prefix, "bin", "kernel" }),
-            },
-        });
-        cp_kernel.step.dependOn(&exe.install_step.?.step);
-        cp_kernel.step.dependOn(&mkdir_isodir.step);
-
-        const generate_grub_cfg = self.createGenerateFileStep(
-            "generate grub.cfg",
-            self.joinPath(&.{ isodir_grub_path, "grub.cfg" }),
-            \\menuentry
-            ++ " " ++ OS_NAME ++ "{" ++
-                \\
-                \\    multiboot /boot/
-            ++ OS_NAME ++ ".bin" ++
-                \\
-                \\}
-            ,
-        );
-        generate_grub_cfg.step.dependOn(&mkdir_isodir.step);
-
-        const iso_location = self.joinPath(&.{ self.builder.install_prefix, "bin", OS_NAME ++ ".iso" });
-
-        const mkiso_step = self.createRunStep("make iso");
-        mkiso_step.addArgs(&.{ "grub-mkrescue", "-o", iso_location, isodir_path });
-        mkiso_step.step.dependOn(&check_iso_prereqs.step);
-        mkiso_step.step.dependOn(&generate_grub_cfg.step);
-        mkiso_step.step.dependOn(&cp_kernel.step);
-
-        const mkiso = self.addStep("mkiso", "Create a bootable ISO image");
-        mkiso.dependOn(&mkiso_step.step);
-
-        const runiso_step = self.createRunStep("run iso");
-        runiso_step.addArgs(&.{ self.qemu, "-cdrom", iso_location });
-        runiso_step.step.dependOn(&mkiso_step.step);
-        runiso_step.step.dependOn(&check_qemu.step);
-
-        const runiso = self.addStep("runiso", "Run the generated iso image in qemu");
-        runiso.dependOn(&runiso_step.step);
-    }
-};
+fn joinPath(paths: []const []const u8) []const u8 {
+    return std.fs.path.join(builder.allocator, paths) catch unreachable;
+}
 
 pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
-    const target = CrossTarget{
-        .cpu_arch = Target.Cpu.Arch.i386,
-        .cpu_model = .{ .baseline = .{} },
-        .os_tag = Target.Os.Tag.freestanding,
-    };
+    config.checkOs();
 
-    var buildManager = Build.create(b, mode, target, "src/linker.ld");
-    buildManager.build();
+    builder = b;
+    mode = builder.standardReleaseOptions();
+    default_ld = FileSource{ .path = "src/linker.ld" };
+
+    const rt = addObject("runtime", "src/runtime.zig");
+
+    const exe = addExecutable("kernel", "src/kernel.zig");
+    exe.addObject(rt);
+    exe.install();
+
+    const check_qemu = createPathDependencyStep("check qemu", &.{qemu});
+
+    const run_step = createRunStep("run qemu");
+    run_step.addArgs(&.{ qemu, "-kernel" });
+    run_step.addArtifactArg(exe);
+    run_step.step.dependOn(&exe.install_step.?.step);
+    run_step.step.dependOn(&check_qemu.step);
+
+    const run = addStep("run", "Run the kernel in qemu");
+    run.dependOn(&run_step.step);
+
+    const check_iso_prereqs = createPathDependencyStep(
+        "check iso creation prerequisites",
+        &.{ "xorriso", "mformat", "grub-mkrescue" },
+    );
+    _ = check_iso_prereqs;
+
+    const isodir_path = joinPath(&.{ builder.install_prefix, "isodir" });
+    const isodir_boot_path = joinPath(&.{ isodir_path, "boot" });
+    const isodir_grub_path = joinPath(&.{ isodir_boot_path, "grub" });
+
+    const mkdir_isodir = createFsStep("mkdir isodir", .{
+        .mkdir = .{
+            .path = isodir_grub_path,
+            .parents = true,
+        },
+    });
+
+    const cp_kernel = createFsStep("cp kernel", .{
+        .cp = .{
+            .to = joinPath(&.{ isodir_boot_path, config.OS_NAME ++ ".bin" }),
+            .from = joinPath(&.{ builder.install_prefix, "bin", "kernel" }),
+        },
+    });
+    cp_kernel.step.dependOn(&exe.install_step.?.step);
+    cp_kernel.step.dependOn(&mkdir_isodir.step);
+
+    const generate_grub_cfg = createGenerateFileStep(
+        "generate grub.cfg",
+        joinPath(&.{ isodir_grub_path, "grub.cfg" }),
+        \\menuentry
+        ++ " " ++ config.OS_NAME ++ "{" ++
+            \\
+            \\    multiboot /boot/
+        ++ config.OS_NAME ++ ".bin" ++
+            \\
+            \\}
+        ,
+    );
+    generate_grub_cfg.step.dependOn(&mkdir_isodir.step);
+
+    const iso_location = joinPath(&.{ builder.install_prefix, "bin", config.OS_NAME ++ ".iso" });
+
+    const mkiso_step = createRunStep("make iso");
+    mkiso_step.addArgs(&.{ "grub-mkrescue", "-o", iso_location, isodir_path });
+    mkiso_step.step.dependOn(&check_iso_prereqs.step);
+    mkiso_step.step.dependOn(&generate_grub_cfg.step);
+    mkiso_step.step.dependOn(&cp_kernel.step);
+
+    const mkiso = addStep("mkiso", "Create a bootable ISO image");
+    mkiso.dependOn(&mkiso_step.step);
+
+    const runiso_step = createRunStep("run iso");
+    runiso_step.addArgs(&.{ qemu, "-cdrom", iso_location });
+    runiso_step.step.dependOn(&mkiso_step.step);
+    runiso_step.step.dependOn(&check_qemu.step);
+
+    const runiso = addStep("runiso", "Run the generated iso image in qemu");
+    runiso.dependOn(&runiso_step.step);
 }
---
+++ build/config.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const CrossTarget = std.zig.CrossTarget;
+const Target = std.Target;
+
+pub const OS_NAME = "adelpha";
+
+pub const target = CrossTarget{
+    .cpu_arch = Target.Cpu.Arch.i386,
+    .cpu_model = .{ .baseline = .{} },
+    .os_tag = Target.Os.Tag.freestanding,
+};
+
+pub const supported_host_os: []std.Target.Os.Tag = .{std.Target.Os.Tag.linux};
+
+pub fn checkOs() void {
+    const os: std.Target.Os = @import("builtin").os;
+    comptime if (!(os.tag.isBSD() or os.tag == .linux))
+        @compileError("\n\n" ++ std.meta.tagName(os.tag) ++
+            \\ is not a supported host OS.
+            \\You could try to rebuild without this check, but chances of success are pretty much 0.
+            \\
+            \\Supported platforms are any form of Linux or BSD (including MacOS).
+            \\
+            \\
+        );
+}
